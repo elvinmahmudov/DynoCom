ba2d51cd2fd5eccc2ceb955961e40382a2256ec4
Elvin Mahmudov <elvin.mahmudov@pashabank.az>
2019-09-14 19:16:43 +0400

    Add teacher model

---
+++ src/test/java/com/elvinmahmudov/dynocom/model/Teacher.java
@@ -0,0 +1,18 @@
+package com.elvinmahmudov.dynocom.model;
+
+import lombok.Data;
+
+/**
+ * Sample class as JavaBean.
+ *
+ * @author elvinmahmudov
+ */
+@Data
+public class Teacher {
+
+    private String id;
+
+    private String designation;
+
+    private String collegeName;
+}
