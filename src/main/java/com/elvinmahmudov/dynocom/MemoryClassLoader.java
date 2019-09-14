776a083aa765a4fba2fec64fd2e851c3309a1f9f
Elvin Mahmudov <elvin.mahmudov@pashabank.az>
2019-09-14 18:49:31 +0400

    Fix format

--- src/main/java/com/elvinmahmudov/dynocom/MemoryClassLoader.java
+++ src/main/java/com/elvinmahmudov/dynocom/MemoryClassLoader.java
@@ -7,26 +7,26 @@ import java.util.Map;
 
 /**
  * Load class from byte[] which is compiled in memory.
- * 
+ *
  * @author elvinmahmudov
  */
 class MemoryClassLoader extends URLClassLoader {
 
-	Map<String, byte[]> classBytes = new HashMap<>();
+    Map<String, byte[]> classBytes = new HashMap<>();
 
-	public MemoryClassLoader(Map<String, byte[]> classBytes) {
-		super(new URL[0], MemoryClassLoader.class.getClassLoader());
-		this.classBytes.putAll(classBytes);
-	}
+    public MemoryClassLoader(Map<String, byte[]> classBytes) {
+        super(new URL[0], MemoryClassLoader.class.getClassLoader());
+        this.classBytes.putAll(classBytes);
+    }
 
-	@Override
-	protected Class<?> findClass(String name) throws ClassNotFoundException {
-		byte[] buf = classBytes.get(name);
-		if (buf == null) {
-			return super.findClass(name);
-		}
-		classBytes.remove(name);
-		return defineClass(name, buf, 0, buf.length);
-	}
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] buf = classBytes.get(name);
+        if (buf == null) {
+            return super.findClass(name);
+        }
+        classBytes.remove(name);
+        return defineClass(name, buf, 0, buf.length);
+    }
 
 }
--- src/main/java/com/elvinmahmudov/dynocom/MemoryJavaFileManager.java
+++ src/main/java/com/elvinmahmudov/dynocom/MemoryJavaFileManager.java
@@ -1,5 +1,11 @@
 package com.elvinmahmudov.dynocom;
 
+import javax.tools.FileObject;
+import javax.tools.ForwardingJavaFileManager;
+import javax.tools.JavaFileManager;
+import javax.tools.JavaFileObject;
+import javax.tools.JavaFileObject.Kind;
+import javax.tools.SimpleJavaFileObject;
 import java.io.ByteArrayOutputStream;
 import java.io.FilterOutputStream;
 import java.io.IOException;
@@ -9,87 +15,80 @@ import java.nio.CharBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.tools.FileObject;
-import javax.tools.ForwardingJavaFileManager;
-import javax.tools.JavaFileManager;
-import javax.tools.JavaFileObject;
-import javax.tools.JavaFileObject.Kind;
-import javax.tools.SimpleJavaFileObject;
-
 /**
  * In-memory java file manager.
- * 
+ *
  * @author elvinmahmudov
  */
 class MemoryJavaFileManager extends ForwardingJavaFileManager<JavaFileManager> {
 
-	final Map<String, byte[]> classBytes = new HashMap<>();
+    final Map<String, byte[]> classBytes = new HashMap<>();
 
-	MemoryJavaFileManager(JavaFileManager fileManager) {
-		super(fileManager);
-	}
+    MemoryJavaFileManager(JavaFileManager fileManager) {
+        super(fileManager);
+    }
 
-	public Map<String, byte[]> getClassBytes() {
-		return new HashMap<>(this.classBytes);
-	}
+    public Map<String, byte[]> getClassBytes() {
+        return new HashMap<>(this.classBytes);
+    }
 
-	@Override
-	public void flush() {
-	}
+    @Override
+    public void flush() {
+    }
 
-	@Override
-	public void close() {
-		classBytes.clear();
-	}
+    @Override
+    public void close() {
+        classBytes.clear();
+    }
 
-	@Override
-	public JavaFileObject getJavaFileForOutput(JavaFileManager.Location location, String className, Kind kind,
-			FileObject sibling) throws IOException {
-		if (kind == Kind.CLASS) {
-			return new MemoryOutputJavaFileObject(className);
-		} else {
-			return super.getJavaFileForOutput(location, className, kind, sibling);
-		}
-	}
+    @Override
+    public JavaFileObject getJavaFileForOutput(JavaFileManager.Location location, String className, Kind kind,
+                                               FileObject sibling) throws IOException {
+        if (kind == Kind.CLASS) {
+            return new MemoryOutputJavaFileObject(className);
+        } else {
+            return super.getJavaFileForOutput(location, className, kind, sibling);
+        }
+    }
 
-	JavaFileObject makeStringSource(String name, String code) {
-		return new MemoryInputJavaFileObject(name, code);
-	}
+    JavaFileObject makeStringSource(String name, String code) {
+        return new MemoryInputJavaFileObject(name, code);
+    }
 
-	static class MemoryInputJavaFileObject extends SimpleJavaFileObject {
+    static class MemoryInputJavaFileObject extends SimpleJavaFileObject {
 
-		final String code;
+        final String code;
 
-		MemoryInputJavaFileObject(String name, String code) {
-			super(URI.create("string:///" + name), Kind.SOURCE);
-			this.code = code;
-		}
+        MemoryInputJavaFileObject(String name, String code) {
+            super(URI.create("string:///" + name), Kind.SOURCE);
+            this.code = code;
+        }
 
-		@Override
-		public CharBuffer getCharContent(boolean ignoreEncodingErrors) {
-			return CharBuffer.wrap(code);
-		}
-	}
+        @Override
+        public CharBuffer getCharContent(boolean ignoreEncodingErrors) {
+            return CharBuffer.wrap(code);
+        }
+    }
 
-	class MemoryOutputJavaFileObject extends SimpleJavaFileObject {
-		final String name;
+    class MemoryOutputJavaFileObject extends SimpleJavaFileObject {
+        final String name;
 
-		MemoryOutputJavaFileObject(String name) {
-			super(URI.create("string:///" + name), Kind.CLASS);
-			this.name = name;
-		}
+        MemoryOutputJavaFileObject(String name) {
+            super(URI.create("string:///" + name), Kind.CLASS);
+            this.name = name;
+        }
 
-		@Override
-		public OutputStream openOutputStream() {
-			return new FilterOutputStream(new ByteArrayOutputStream()) {
-				@Override
-				public void close() throws IOException {
-					out.close();
-					ByteArrayOutputStream bos = (ByteArrayOutputStream) out;
-					classBytes.put(name, bos.toByteArray());
-				}
-			};
-		}
+        @Override
+        public OutputStream openOutputStream() {
+            return new FilterOutputStream(new ByteArrayOutputStream()) {
+                @Override
+                public void close() throws IOException {
+                    out.close();
+                    ByteArrayOutputStream bos = (ByteArrayOutputStream) out;
+                    classBytes.put(name, bos.toByteArray());
+                }
+            };
+        }
 
-	}
+    }
 }
