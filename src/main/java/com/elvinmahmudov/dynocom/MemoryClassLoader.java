c0e4caa1cac72c8805fa130c9708c74d23212716
Elvin Mahmudov <elvin.mahmudov@pashabank.az>
2019-09-14 18:48:20 +0400

    Add java memory class loader

---
+++ src/main/java/com/elvinmahmudov/dynocom/MemoryClassLoader.java
@@ -0,0 +1,32 @@
+package com.elvinmahmudov.dynocom;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Load class from byte[] which is compiled in memory.
+ * 
+ * @author elvinmahmudov
+ */
+class MemoryClassLoader extends URLClassLoader {
+
+	Map<String, byte[]> classBytes = new HashMap<>();
+
+	public MemoryClassLoader(Map<String, byte[]> classBytes) {
+		super(new URL[0], MemoryClassLoader.class.getClassLoader());
+		this.classBytes.putAll(classBytes);
+	}
+
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		byte[] buf = classBytes.get(name);
+		if (buf == null) {
+			return super.findClass(name);
+		}
+		classBytes.remove(name);
+		return defineClass(name, buf, 0, buf.length);
+	}
+
+}
